398f44ccac1efc4f7564e3a1b08e030fd3062628
James Dineley <jdineley@hotmail.com>
2024-07-15 17:15:13 +0100

    new tutorial links to louder videos

--- frontend/src/pages/Learning.jsx
+++ frontend/src/pages/Learning.jsx
@@ -4,35 +4,35 @@ const Learning = () => {
   return (
     <div>
       <a
-        href="https://youtu.be/7xbZxUh-BdE"
+        href="https://youtu.be/VmMYp2oiDcs"
         target="_blank"
         className="block mb-2"
       >
         1. Beginner's introduction: Users, Projects, Tasks
       </a>
       <a
-        href="https://youtu.be/Zz_lfVvuVtg"
+        href="https://youtu.be/1bXMdWaUqLc"
         target="_blank"
         className="block mb-2"
       >
         2. The basics of collaboration
       </a>
       <a
-        href="https://youtu.be/p1Y15sxzLAU"
+        href="https://youtu.be/AYxSah6yviY"
         target="_blank"
         className="block mb-2"
       >
         3. Multiple user collaboration & Project Reviews
       </a>
       <a
-        href="https://youtu.be/XbK0WZwmUwQ"
+        href="https://youtu.be/MY3Sb0lJmw4"
         target="_blank"
         className="block mb-2"
       >
         4. Managing user vacations
       </a>
       <a
-        href="https://youtu.be/23BmefoUgz0"
+        href="https://youtu.be/POOGlQWMS8c"
         target="_blank"
         className="block mb-2"
       >
