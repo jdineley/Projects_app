b8d62ba38263bd51d5edb30c74b0d2a08988a99b
James Dineley <jdineley@hotmail.com>
2024-07-19 22:55:53 +0100

    image width correction

--- frontend/src/pages/Learning.jsx
+++ frontend/src/pages/Learning.jsx
@@ -50,11 +50,11 @@ const Learning = () => {
           </a>
         </div>
         <div className={`object-contain ${!isTabletResolution && "w-7/12"}`}>
-          <img src="/Projects flyer.1.jpeg" className="w-full min-w-96" />
+          <img src="/Projects flyer.1.jpeg" className="w-full" />
         </div>
       </div>
       <div className="object-contain">
-        <img src="/Project short 2_cropped.gif" className="w-full min-w-96" />
+        <img src="/Project short 2_cropped.gif" className="w-full" />
       </div>
     </div>
   );
