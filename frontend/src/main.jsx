832014e3de028b2a94d2d9c259dd54e1c3b9651c
James Dineley <jdineley@hotmail.com>
2024-07-20 16:36:09 +0100

    sentry initialization

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,22 @@ import { NotificationContextProvider } from "./context/NotificationContext.jsx";
 import { Theme } from "@radix-ui/themes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://ae5e8036ccf9064dd9c2b48841ccdb20@o4506342901940224.ingest.us.sentry.io/4507628979945472",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
