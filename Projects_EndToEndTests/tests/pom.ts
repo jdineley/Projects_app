e17486c2ffe8e59771bfb7c00e5dd4042f76df67
James Dineley <jdineley@hotmail.com>
2024-11-29 14:34:41 +0000

    increase addMSProject timeout to 200s

--- Projects_EndToEndTests/tests/pom.ts
+++ Projects_EndToEndTests/tests/pom.ts
@@ -190,7 +190,7 @@ export class ProjectsPage {
       this.page.getByRole("link", {
         name: xmlName,
       })
-    ).toBeVisible({ timeout: 100000 });
+    ).toBeVisible({ timeout: 200000 });
   }
 }
 
