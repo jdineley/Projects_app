679cff714a32f36f5ff0919c6cb10b036c8c2461
James Dineley <jdineley@hotmail.com>
2024-12-04 13:28:37 +0000

    add --reporter=html flag and path: playwright-report/*

--- Projects_EndToEndTests/playwright.config.ts
+++ Projects_EndToEndTests/playwright.config.ts
@@ -33,8 +33,9 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    // trace: "on-first-retry",
-    trace: "retain-on-failure",
+    trace: "on-first-retry",
+    // trace: "retain-on-failure",
+    // trace: "on",
   },
 
   /* Configure projects for major browsers */
